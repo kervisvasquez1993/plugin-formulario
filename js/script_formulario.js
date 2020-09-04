faee28dc7f7023c399249900b475c055a6017f18
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-09-04 08:49:46 -0400

    agregamos imagenes al formulario

--- js/script_formulario.js
+++ js/script_formulario.js
@@ -1,28 +1,35 @@
+
 ((d,c,$) =>
-{
+{	    
 		document.addEventListener('DOMContentLoaded', () => {
-          let dias                  = 1,
-          ciclo_conferencista       = 0
-          dias_ciclos               = Number(dias - 1)
-		  dias_string               = ` Dia ${dias}`,
-		  contador_dia              = 0,
-          content_dias              = document.querySelectorAll('.dias-ciclos'),
-          // añadir dias
-          dia_padre                 = document.querySelector('.wrap-dia-padre'),
-          dia_hija                  = document.querySelector('.wrap-day-child'),
-          //conferencista   
-          conferencista_padre       = document.querySelector('.wrap-conferencista-padre'),
-          conferencista_hijo        = document.querySelector('.wrapp-conferencista-child'),
-          //moderador   
-          moderador_padre           = document.querySelector('.wrap-moderador-padre'),
-          moderador_hijo            = document.querySelector('.wrap-moderadores-child'),
-		  //botones   
-		  anadir_dias               = document.querySelector('.anadir-dia'),
-          anadir_moderador_all      = document.querySelector('.boton-añadir-moderador'),
-		  anadir_conferencista_all  = document.querySelector('.boton-añadir-conferencista'),
 		  
-		  //moderador_array           = Array.from(anadir_moderador_all),
-		  //conferencista       = Array.from(anadir_conferencista_all)
+		  form = document.querySelector('.wpcf7-form')
+		  console.log(form)
+		  form.classList.add('border-botones')	
+		  let dias = 1,
+          ciclo_conferencista = 0
+          dias_ciclos = Number(dias - 1)
+		  dias_string             = ` Dia ${dias}`,
+		  
+          content_dias            = document.querySelectorAll('.dias-ciclos'),
+          
+          // añadir dias
+         
+          dia_padre               = document.querySelector('.wrap-dia-padre'),
+          dia_hija                = document.querySelector('.wrap-day-child'),
+          //conferencista   
+          conferencista_padre     = document.querySelectorAll('.wrap-conferencista-padre'),
+          conferencista_hijo      = document.querySelectorAll('.wrapp-conferencista-child'),
+          //moderador   
+          moderador_padre         = document.querySelectorAll('.wrap-moderador-padre'),
+          moderador_hijo          =document.querySelectorAll('.wrap-moderadores-child'),
+		  //botones   
+		  anadir_dias             = document.querySelector('.anadir-dia'),
+          anadir_moderador_all = document.querySelectorAll('.boton-añadir-moderador'),
+		  anadir_conferencista_all = document.querySelectorAll('.boton-añadir-conferencista'),
+		  contador_dia = 0,
+		  moderador_array = Array.from(anadir_moderador_all),
+		  conferencista_array = Array.from(anadir_conferencista_all)
 		  
           content_dias[0].innerHTML = dias_string
           //console.log(content_dias[0])
@@ -31,29 +38,27 @@
 		  function ciclo(elemento_hijo,elemento_padre)
          {
 		    ciclo_conferencista += 1
-			let clon_hijo = elemento_hijo.cloneNode(elemento_hijo)
-			clon_hijo.classList.add('test')
-			elemento_padre.appendChild(clon_hijo)
+            let clon_hijo = elemento_hijo.cloneNode(elemento_hijo)
 			let input = document.querySelectorAll('.test:last-child .wpcf7-form-control-wrap .wpcf7-form-control')
-			inputArr = Array.from(input)
-			console.log(inputArr)
+			clon_hijo.classList.add('test')
+		    elemento_padre.appendChild(clon_hijo)
+            inputArr = Array.from(input)
 			inputArr.forEach(i =>
-			{  
-			   name_input = i.getAttribute('name')
+			{
+               name_input = i.getAttribute('name')
                let conferencista_name_atribute = `${name_input}_${ciclo_conferencista}`
                i.setAttribute('name', conferencista_name_atribute )
-			   i.value = ''
-			   
+               i.value = ''
 		    })
 		 }
 	   
 	   function ciclo_dias(e)
 	   {
-            e.preventDefault()
+			e.preventDefault()
+			contador_dia += 1
 		    let clon_dia = dia_hija.cloneNode(dia_hija)
 			clon_dia.classList.add('siguiente_dia')
-			console.log(dia_padre.appendChild(clon_dia))
-			
+			dia_padre.appendChild(clon_dia)
 			let campos_eliminar_repetido = document.querySelectorAll('.siguiente_dia .grupos-comun  .wrap-comun ')
 			let campos_eliminar_repetido2 = document.querySelectorAll('.siguiente_dia .grupos-comun  .wrap-comun .test')
 			//console.log(campos_eliminar_repetido)
@@ -62,8 +67,10 @@
 			campos_eliminar_repetido2.forEach(a => {
 				a.remove()
             })
+
+            
             // variables conferencista padre e hijas
-			/*conferencista_padre     = document.querySelectorAll('.wrap-conferencista-padre'),
+			conferencista_padre     = document.querySelectorAll('.wrap-conferencista-padre'),
             conferencista_hijo      = document.querySelectorAll('.wrapp-conferencista-child'),
             // variables moderadores padre e hijas
 			moderador_padre         = document.querySelectorAll('.wrap-moderador-padre'),
@@ -73,7 +80,8 @@
 		    anadir_conferencista_all = document.querySelectorAll('.boton-añadir-conferencista'),
 		    moderador_array = Array.from(anadir_moderador_all),
 			conferencista_array = Array.from(anadir_conferencista_all)
-			anadir_conferencista_all
+			
+			
 			// eventos para agregar moderadores  
 	        moderador_array[contador_dia].addEventListener('click', (e)=>
 			{
@@ -86,31 +94,29 @@
 			conferencista_array[contador_dia].addEventListener('click', (e)=>{
 				e.preventDefault()
 			ciclo(conferencista_hijo[contador_dia], conferencista_padre[contador_dia])
-			
 		  })
        
-            */
+            console.log(contador_dia)
         }
         
 
           //llamar a eventos 
 	      anadir_dias.addEventListener('click', ciclo_dias)
+          //eventos conferncista
+		  
 		 
-		 
-		 
-		  //eventos conferncista
-          anadir_conferencista_all.addEventListener('click', (e)=>{
+		  conferencista_array[0].addEventListener('click', (e)=>{
                 e.preventDefault()
-            ciclo(conferencista_hijo, conferencista_padre)
+            ciclo(conferencista_hijo[0], conferencista_padre[0])
           })
 	   
 	   
 
          //eventos moderador
-         anadir_moderador_all.addEventListener('click', (e)=>
+         moderador_array[0].addEventListener('click', (e)=>
          {
                e.preventDefault()
-           ciclo(moderador_hijo, moderador_padre)
+           ciclo(moderador_hijo[0], moderador_padre[0])
          })
          
          
@@ -119,3 +125,4 @@
        
 })
 (document,console.log,jQuery.noConflict())
+
--- test-formulario.php
+++ test-formulario.php
@@ -1,25 +1,28 @@
 <div class="container">
  <h5 class="text-center">Queremos que seas parte de nosotros Inscribete Aquí </h5>
-
-      [select* patrocinante class:custom-select  "Patrocinante" "Dynamics" "Surgery" "Dental"]
-
+       <label class="col-12 col-sm-12">
+           [select* patrocinante class:custom-select  "Patrocinante" "Dynamics" "Surgery" "Dental"]
+       </label>
       <p class="red text-center bg-danger">SELECIONALA MODALIDAD</p>
 
-      [radio modalidad label_first default:2 "Ciclo" "Conferencista"]
+       [radio modalidad label_first default:2 "Ciclo" "Conferencista"]
 
 [group Conferencista]
 
 [/group]
       
-[group Ciclo]
-<div class="grupo1-cabecera">
-              <span class="resaltado-titulo"> CICLO </class>
+  [group Ciclo]
+
+
+        <div class="grupo1-cabecera  row">
+              <span class="resaltado-titulo col"> CICLO </class>
            
-              <label> Titulo (*)
+              <label class="col-12 col-sm-12"> 
+                 Titulo (*)
                  [text* titulo placeholder "Titulo"]
               </label>
-              <label> Área de Especialización  (*)
-                 [select readeEspecializacin "area1" "area2" "area3" "area4"]
+              <label class="col-12 col-sm-12"> Área de Especialización  (*)
+                       [select* especializacion class:especializacion include_blank "area1" "area2" "area3" "area4" "area5"]
               </label>
         </div>
        <!--FIN DE TITULO -->
@@ -31,74 +34,85 @@
                 <span class="dias-ciclos"></span>
                 <!-- ========CONFERENCIA ======= -->
                 <div class="grupo1-conferencia">
-                 <span class="resaltado-titulo">CONFERENCIA</span>
-                     <label> 
-                        [text titulo-conferencia placeholder "Título"]
+                 <span class="resaltado-titulo text-center">CONFERENCIA</span>
+                     <label class="col-12 col-sm-12"> 
+                        [text titulo-conferencia class:border-botones  placeholder "Título"]
                      </label>
-                     <label>
-                        [textarea descripcion-conferencia placeholder "Descripción"] 
+                     <label class="col-12 col-sm-12">
+                        [textarea descripcion-conferencia class:border-botones placeholder "Descripción"] 
                      </label>
-                </div> 
+            </div> 
                 <!-- =========== FIN DE CONFERENCIA ============ -->
                 <!-- ========== INICIO CONFERENCISTA ===== -->
-                <div class="grupos-comun grupo1-conferencista">
-                    <div class="wrap-comun wrap-conferencista-padre"> 
-                        <div class="wrap-comun-chil wrapp-conferencista-child">
-                      <span class="resaltado-titulo">CONFERENCISTA</span>
-                      <label>
-                         [text nombre placeholder "Nombre"]
-                      </label>
-                      <label>
-                         [tel telefono placeholder "teléfono"]
-                      </label> 
-                      <label>
-                         [email email placeholder "Email"]
-                      </label>
-                      <label>
-                         [text pais_origen placeholder "País de Origen"]
-                      </label>
-                      <label>
-                         [text* pais_residencia placeholder "País de 
-                         Residencia"]
-                      </label>
+               <div class="grupos-comun grupo1-conferencista">
+                     <div class="wrap-comun wrap-conferencista-padre container"> 
+                        <div class="wrap-comun-chil wrapp-conferencista-child row">
+                                   <span class="resaltado-titulo">CONFERENCISTA</span>
+                                   <label class="col-12 col-sm-12">
+                                      [text* nombre_conferencista class:border-botones  placeholder "Nombre"]
+                                   </label>
+                                   <label class="col-12 col-sm-5">
+                                      [tel* telefono_conferencista class:border-botones placeholder "teléfono"]
+                                   </label> 
+                                   <label class="col-12 col-sm-7">
+                                      [email* email_conferencista class:border-botones placeholder "Email"]
+                                   </label>
+                                   <label class="col-12 col-sm-12">
+                                      [text* especializacion_conferencista class:border-botones placeholder "Especialización"]
+                                   </label>
+                                   <label class="col-12 col-sm-6">
+                                      [text* pais_origen_conferencista  class:border-botones placeholder "País de Origen"]
+                                   </label>
+                                   <label class="col-12 col-sm-6">
+                                      [text* pais_residencia_conferencista  class:border-botones placeholder "País de 
+                                      Residencia"]
+                                   </label>
              
-                      <div class="adjuntar_informacion">
-                           <label>
-                               Adjuntar CV
-                              [file* adjuntar_cv]
-                           </label>
-                           <label>
-                                Fotografía
-                                [file fotografia]
-                           </label>
-                      </div>
+                                   <div class="col col-12 adjuntar_informacion row">
+                                        <label class="col">
+                                            Adjuntar CV
+                                           [file* adjuntar_cv_conferencista]
+                                        </label>
+                                        <label class="col">
+                                             Fotografía
+                                             [file fotografia_conferencista]
+                                        </label>
+                                   </div>
                         </div>
-                    </div>
-                   <!--============ inicio de boton para añadir conferencista-->
-                   <p class="boton-añadir-conferencista">Añadir Conferencista</p>
-                   <!-- ========== fin de boton ========== ------>
-                </div> 
+                     </div>
+                     <!--============ inicio de boton para añadir conferencista-->
+                     <p class="boton-añadir-conferencista">Añadir Conferencista</p>
+                     <!-- ========== fin de boton ========== ------>
+               </div> 
                 <!--========FIN DE CONFERENCISTA ====== -->
                 <!-- ========== MODERADORES =========== ------>
                 <div class="grupos-comun grupo1-moderador">
-                    <div class="wrap-comun wrap-moderador-padre"> 
-                         <div class=" wrap-comun-chil wrap-moderadores-child">
-                         <span class="resaltado">MODERADOR</span>
-                         [text* nombre placeholder placeholder "Nombre"]
-                         [tel* telefono-moderador placeholder "Teléfono"]
-                         [email correo-moderador placeholder "E-mail"]
-                         [text especializacion-moderador placeholder "Especialización"]
-                         <div class="adjuntar_informacion">
-                           <label>
-                               Adjuntar CV
-                              [file* adjuntar_cv]
-                           </label>
-                           <label>
-                                Fotografía
-                                [file fotografia]
-                           </label>
-                         </div>
-                         </div>
+                    <div class="wrap-comun wrap-moderador-padre container"> 
+                         <div class=" wrap-comun-chil wrap-moderadores-child row">
+                                <span class="resaltado col-12 col-sm-12">MODERADOR</span>
+                                <label  class="col-12 col-sm-12">
+                                    [text* nombre_moderador class:border-botones  placeholder "Nombre"]
+                                </label>
+                                <label class="col-12 col-sm-5">
+                                    [tel* telefono_moderador class:border-botones placeholder "Teléfono"]
+                                </label>
+                                <label class="col-12 col-sm-7">
+                                    [email correo_moderador class:border-botones placeholder "E-mail"]
+                                </label>
+                                <label class="col col-12">
+                                    [text especializacion_moderador class:border-botones placeholder "Especialización"]
+                                </label>
+                                <div class="adjuntar_informacion col col-12">
+                                  <label>
+                                      Adjuntar CV
+                                     [file* adjuntar_cv_moderador]
+                                  </label>
+                                  <label>
+                                       Fotografía
+                                       [file fotografia_moderador]
+                                  </label>
+                                </div>
+                          </div>
                     </div>    
                     <!--============ inicio de boton para añadir moderadores-->
                     <p class="boton-añadir-moderador">Añadir Moderador</p>
@@ -108,7 +122,7 @@
         
       </div>
       <p class="anadir-dia">Añadir Dia</p>
-   </div>
+</div>
 [/group]
 
 [submit "Enviar"]
